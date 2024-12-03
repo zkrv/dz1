680b3bc029d975b3bf8c190297239976ca6bdb87
Admin <zakirovrahim46@gmail.com>
2024-12-03 23:24:27 +0600

    dz1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+function Header() {
+    return (
+        <>
+        <h1>For head</h1>
+            <Title />
+        </>
+    )
+}
+function Content() {
+    return (
+        <>
+        <h3>For content</h3>
+            <Title />
+        </>
+    )
+}
+function Footer() {
+    return (
+        <>
+        <p>For footer</p>
+            <Title />
+        </>
+    )
+}
+function Title(){
+    return (
+        <p>For title</p>
+    )
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <Content />
+            <Footer />
+
+        </div>
+    );
 }
 
 export default App;
